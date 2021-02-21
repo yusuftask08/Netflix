e0374a47a47ebcda1cd18d5870c52429823d4ffb
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-02-21 17:14:13 +0300

    v2.5 modal bitti sayfa düzenlemesi yapildi. login işlemleri basliyor

--- src/components/Home/sliderHomeTwo.vue
+++ src/components/Home/sliderHomeTwo.vue
@@ -1,35 +1,135 @@
 <template>
-  <div style="height: 450px; overflow-x: scroll; overflow-y: hidden " >
-    <div class="text-span">
-      <span> Outside the Wire Adlı Yapımı İzleyenlere Öneriler    </span>
-    </div>
-    <div class="container-card">
-      <div class="imdb" v-for="topTenList in topTenList" :key="topTenList.id">
-        <div class="ms-3 movie_card">
-          <img
-            :src="'http://image.tmdb.org/t/p/w500/' + topTenList.poster_path"
-            width="100px"
-            class="card-img-top"
-            alt="..."
-          />
-          <!-- <h3 class="title text-white">{{ seriesList.original_title }}</h3> -->
-          <p class="text-white">{{ topTenList.vote_count }}</p>
-          <!-- <p class="text-white"> {{ seriesList.overview }}</p> -->
+  <div class="text-span">
+    <span> Outside the Wire Adlı Yapımı İzleyenlere Öneriler </span>
+  </div>
+  <swiper
+    :slides-per-view="7"
+    :space-between="0"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+    :mousewheel-control="false"
+    :performance-mode="false"
+    :pagination-visible="true"
+    :pagination-clickable="false"
+    :autoScrollOffset="0"
+    :draggable="false"
+    :mousewheel="false"
+    navigation
+    class="item swiper-scrollbar-lock ms-5"
+  >
+    <swiper-slide v-for="OutOnerilenler in OutOnerilenler" :key="OutOnerilenler.id">
+      <div class="ms-2 me-2 movie_card item">
+        <img
+          :src="'http://image.tmdb.org/t/p/w500/' + OutOnerilenler  .poster_path"
+          class="card-img-top"
+          alt="..."
+        />
+
+        <div class="film_info">
+          <div class="list-icon-left">
+            <a
+              ><i class="fas fa-play" style="background: white; color: black">
+              </i
+            ></a>
+
+            <a
+              ><i class="fas fa-plus toolTip">
+                <span class="toolTiptext-sm tool-span-sm"> Listeme ekle </span>
+              </i></a
+            >
+            <a @click="showDetail(OutOnerilenler)">
+              <i class="fas fa-chevron-down toolTip">
+                <span class="toolTiptext-sm tool-span-sm">
+                  Daha fazla bilgi
+                </span>
+              </i>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+    </swiper-slide>
+  </swiper>
+
+  <modals
+    :modalData="modalData"
+    :modalId="modalId"
+    :videoId="videoId"
+    v-model="isOpen"
+  />
 </template>
+
 <script>
 import axios from "axios";
+import modals from "@/components/modal/modals";
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+import SwiperCore, { Navigation, Pagination, A11y } from "swiper";
 
+// Import Swiper styles
+import "swiper/components/navigation/navigation.scss";
+import "swiper/swiper.scss";
 export default {
+  components: {
+    modals,
+    Swiper,
+    SwiperSlide,
+    Navigation,
+  },
   data() {
     return {
-      topTenList: [],
+      OutOnerilenler: [],
+      isOpen: false,
+      modalData: null,
+      modalId: null,
+      videoId: [],
+      showLoading: true,
     };
   },
+  methods: {
+    async showDetail(OutOnerilenler) {
+      console.log("isOpenACILIYORMU :>> ", this.isOpen);
+      this.showLoading = true;
+      try {
+        await axios
+          .get(
+            `https://api.themoviedb.org/3/movie/${OutOnerilenler.id}?api_key=7b97ca5600ae944d697e04e778928d05&language=en-US&append_to_response=videos,credits,release_dates,similar`
+          )
+          .then((response) => {
+            console.log("iddetailvideos", response);
+            this.modalId = response.data;
+            this.modalData = OutOnerilenler;
+          });
+
+        console.log("netflixTePop", OutOnerilenler);
+        this.isOpen = true;
+
+        this.videoId = this.modalId.videos.results;
+        console.log("this.videoId :>> ", this.videoId);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.showLoading = false;
+      }
+    },
+  },
+
   created() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/38700/similar?api_key=7b97ca5600ae944d697e04e778928d05&language=en-US&page=1"
+      )
+      .then((response) => {
+        console.log("topTenList", response);
+        this.OutOnerilenler = response.data.results;
+      });
+  },
+};
+</script>
+   
+OutOnerilenler
+
+        <span> Outside the Wire Adlı Yapımı İzleyenlere Öneriler    </span>
+created() {
     axios
       .get(
         "https://api.themoviedb.org/3/movie/38700/similar?api_key=7b97ca5600ae944d697e04e778928d05&language=en-US&page=1"
@@ -38,7 +138,4 @@ export default {
         console.log("topTenList", response);
         this.topTenList = response.data.results;
       });
-  },
-};
-</script>
-   
+  },topTenList: [],
